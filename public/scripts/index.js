5c68ba85230d796eda2451925529c3f4924dd3b3
Hedi M Sharif <hedymuhamad461@gmail.com>
2021-11-17 19:29:03 +0300

    Adding Fun-Fact box
    No looking good, still here until breaking down

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -8,28 +8,27 @@ var currentPlayingMode = 'single-player-mode';
 var nextPlayingMode = 'multi-player-mode';
 
 var displayModeControlContainer = $('.display-mode-control-container')[0];
-const myId = (Math.floor((Math.random()*100000))-1);
+const myId = (Math.floor((Math.random() * 100000)) - 1);
 ($('.id-value')[0]).value = myId;
 
 ///////////////////////////////////////////////////////////////
 
-for(let i=0;i<childrenOfThetable.length;i++){
-    const randomChance = Math.floor(Math.random()*2);
-    if(randomChance==0){
+for (let i = 0; i < childrenOfThetable.length; i++) {
+    const randomChance = Math.floor(Math.random() * 2);
+    if (randomChance == 0) {
         childrenOfThetable[i].classList.add('O-marker');
-    }
-    else{
+    } else {
         childrenOfThetable[i].classList.add('X-marker');
     }
 }
 
-modeBorderFunction = (event)=>{
+modeBorderFunction = (event) => {
     $('.mode-border')[0].classList.replace(currentPlayingMode, nextPlayingMode);
-    currentPlayingMode = [nextPlayingMode, nextPlayingMode=currentPlayingMode][0];
+    currentPlayingMode = [nextPlayingMode, nextPlayingMode = currentPlayingMode][0];
 }
 
 
-themeSwitchController = (event) =>{
+themeSwitchController = (event) => {
     console.log(nextTheme);
     console.log(currentTheme);
 
@@ -39,12 +38,21 @@ themeSwitchController = (event) =>{
     $('.mode-border *').css('transition', 'none');
 
     $('body')[0].classList.replace(currentTheme, nextTheme);
+    $('.fun-fact-container')[0].classList.replace(`fun-fact-container-${currentTheme}`, `fun-fact-container-${nextTheme}`);
 
-    
     //re-adding mode-detector child transmission for make theme change more smooth
     setTimeout(() => {
         $('.mode-border *').css('transition', '0.25s ease-in-out');
     }, 0);
-    
-    currentTheme = [nextTheme, nextTheme=currentTheme][0];
+
+    currentTheme = [nextTheme, nextTheme = currentTheme][0];
+}
+
+manageDisplayingFunfact = (event) => {
+
+    if ($('body')[0].clientWidth * 1.45 <= $('body')[0].clientHeight) {
+        $('body')[0].classList.add('display-fun-fact');
+    } else {
+        $('body')[0].classList.remove('display-fun-fact');
+    }
 }
